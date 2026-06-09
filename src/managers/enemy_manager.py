0bc3a20063765702ae7374d2d8b4bf93c1475711
Rémy Ruiz <rruiz@student.42lehavre.fr>
2026-06-09 13:24:39 +0200

    502: bfs implementer, plus que 1 algo a mettre

--- src/managers/enemy_manager.py
+++ src/managers/enemy_manager.py
@@ -6,7 +6,7 @@
 #  By: alebaron, rruiz                           +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/06/02 09:41:40 by rruiz           #+#    #+#               #
-#  Updated: 2026/06/09 09:31:25 by rruiz           ###   ########.fr        #
+#  Updated: 2026/06/09 13:00:50 by rruiz           ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -59,7 +59,7 @@ class EnemyManager:
     def _create_enemies(self) -> list[EnemyModel]:
 
         enemies = []
-        enemies_infos = [blinky, clyde]
+        enemies_infos = [blinky, clyde, inky]
 
         for info in enemies_infos:
             mon = info
